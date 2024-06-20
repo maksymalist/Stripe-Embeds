db28b5ee80481450092fa4e99157e6493a5cd9ef
maksymalist <maksymvovkk@gmail.com>
2024-06-20 02:20:07 -0400

    first commit

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -2,7 +2,7 @@ import { Stripe } from "stripe";
 
 // Function to get Stripe instance
 function getStripe() {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // replace with your actual secret key
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? ""); // replace with your actual secret key
   return stripe;
 }
 
