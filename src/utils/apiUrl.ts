03563113f33df64568c4b1cecf7e49c07d63b6cd
maksymalist <maksymvovkk@gmail.com>
2024-06-20 00:37:52 -0400

    first commit

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -1,8 +1,6 @@
-import { env } from "../env";
-
 const getApiUrl = (path: string) => {
   const apiUrl =
-    env.NODE_ENV === "development"
+    process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : "https://t3-app.vercel.app";
 
--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,9 +1,8 @@
 import { Stripe } from "stripe";
-import { env } from "../env";
 
 // Function to get Stripe instance
 function getStripe() {
-  const stripe = new Stripe(env.STRIPE_SECRET_KEY); // replace with your actual secret key
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as String); // replace with your actual secret key
   return stripe;
 }
 
