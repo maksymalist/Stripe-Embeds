f887f5e057a677ad0425e67264c7ce6e5e529478
maksymalist <maksymvovkk@gmail.com>
2024-06-20 14:44:11 -0400

    created a better looking widget

--- src/pages/auth-redirect.tsx
+++ src/pages/auth-redirect.tsx
@@ -25,6 +25,41 @@ export const getServerSideProps = async (
   const { query } = context;
   const code = query.code;
 
+  try {
+    const response = await fetch("https://connect.stripe.com/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: "YOUR_CLIENT_ID",
+        client_secret: "YOUR_CLIENT_SECRET",
+        code: code as string,
+        grant_type: "authorization_code",
+      }).toString(),
+    });
+
+    const data = (await response.json()) as { access_token: string };
+    const accessToken = data.access_token;
+    return {
+      props: {
+        code: code ? code : "",
+        status: code ? "success" : "error",
+      },
+    };
+  } catch (error) {
+    console.error(
+      "Error exchanging authorization code for access token:",
+      error,
+    );
+    return {
+      props: {
+        code: code ? code : "",
+        status: "error",
+      },
+    };
+  }
+
   return {
     props: {
       code: code ? code : "",
